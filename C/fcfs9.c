d04bac690f68871dc8e23883c5bdd5d1224d8932
Broken <124747342+ajayBhaskar21@users.noreply.github.com>
2023-04-01 17:50:15 +0530

    Update fcfs9.c

--- C/fcfs9.c
+++ C/fcfs9.c
@@ -1,73 +1,86 @@
-#include<stdio.h>
+// FCFS in C, Non-preemptive
+
+#include <stdio.h>
 #include <stdlib.h>
 
-void sortBased(int a[], int b[], int c[], int n){
-	
-	for(int i = 0; i < n-1; i++){
-		for(int j=i+1; j<n;j++){
-			if(c[i] > c[j]){
-				int t1=c[i];
-				c[i]=c[j];
-				c[j]=t1;
-				t1=a[i];
-				a[i]=a[j];
-				a[j]=t1;
-				t1=b[i];
-				b[i]=b[j];
-				b[j]=t1;
+void sortBased(int a[], int b[], int c[], int n)
+{
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (c[i] > c[j])
+			{
+				int t1 = c[i];
+				c[i] = c[j];
+				c[j] = t1;
+				t1 = a[i];
+				a[i] = a[j];
+				a[j] = t1;
+				t1 = b[i];
+				b[i] = b[j];
+				b[j] = t1;
 			}
 		}
 	}
-	
 }
 
+int main()
+{
 
-int main(){
-	
-	
 	int n;
 	printf("Enter number of processess: ");
 	scanf("%d", &n);
 
-	int PID[n],AT[n], BT[n], CT[n], TAT[n], WT[n];
-	
-	for(int i=0;i<n;i++){
-		printf("Enter AT for P%d: ",i+1);
+	int PID[n], AT[n], BT[n], CT[n], TAT[n], WT[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("Enter AT for P%d: ", i + 1);
 		scanf("%d", &AT[i]);
-		printf("Enter BT for P%d: ",i+1);
+		printf("Enter BT for P%d: ", i + 1);
 		scanf("%d", &BT[i]);
 
-		PID[i] = i+1;
+		PID[i] = i + 1;
 	}
-	sortBased(PID, BT, AT); // last argument is sorted and remaining based on the last.
+	sortBased(PID, BT, AT, n); // last argument is sorted and remaining based on the last.
 
- //Completion time.
-	int st=AT[0];
- for(int i=0; i < n; i++){
-  if(AT[i] > st){
-   st=AT[i];
-  }
-  CT[i]=BT[i]+st;
- }
+	// Completion time.
+	int st = AT[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (AT[i] > st)
+		{
+			st = AT[i];
+		}
+		CT[i] = BT[i] + st;
+		st = CT[i];
+	}
 
- //Turn Around Time and Waiting Time
- for(int i=0;i <n;i++){
+	// Turn Around Time and Waiting Time
+	for (int i = 0; i < n; i++)
+	{
 
-  TAT[i]=CT[i]-AT[i];
-  WT[i]=TAT[i]-BT[i];
+		TAT[i] = CT[i] - AT[i];
+		WT[i] = TAT[i] - BT[i];
+	}
 
- }
+	// Avg TAT, Avg WT.
+	float avgTat = 0, avgWt = 0;
+	for (int i = 0; i < n; i++)
+	{
+		avgTat += TAT[i];
+		avgWt += WT[i];
+	}
+	printf("PID\tAT\tBT\tCT\tTAT\tWT\n");
+	for (int i = 0; i < n; i++){
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n", PID[i], AT[i], BT[i], CT[i], TAT[i], WT[i]);
 
- // Avg TAT, Avg WT.
- float avgTat = 0, avgWt=0;
- for(int i=0;i<n;i++){
-  avgTat += TAT[i];
-  avgWt += WT[i];
- }
-  
- float np = (float)n;
- avgTat/=np;
- avgWt/=np;
- printf("%f %f",avgTat,avgWt);
-	
+	}
+
+	float np = (float)n;
+	avgTat /= np;
+	avgWt /= np;
+	printf("%f %f", avgTat, avgWt);
 }
